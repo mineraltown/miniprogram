2af8f3b9f111c93f26ec094d0b4a112da570ab48
MisakaAI <i@zi-o.com>
2025-04-10 15:02:09 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+App({
+    // 小程序初始化完成时触发，全局只触发一次。
+    onLaunch() {},
+    onShow() {},
+    globalData: {
+        url: "http://api.mineraltown.net:8888/",
+        version: {},
+        index: 0,
+    }
+})
